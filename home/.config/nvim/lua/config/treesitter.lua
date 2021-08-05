41ce5aca0c00cfe474bbc0a6b809753cd059a73c
Chad Capra <chadcapra@gmail.com>
2021-08-05 16:40:30 -0500

    leave lsp out of config for now

--- home/.config/nvim/lua/config/treesitter.lua
+++ home/.config/nvim/lua/config/treesitter.lua
@@ -1,7 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "elixir",
-    "typescript"
+    "typescript",
+    "lua",
   },
   highlight = {
     enable = true,
@@ -19,6 +20,39 @@ require'nvim-treesitter.configs'.setup {
       scope_incremental = "grc",
       node_decremental = "grm",
     }
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
   },
   playground = {
     enable = true,
@@ -39,3 +73,6 @@ require'nvim-treesitter.configs'.setup {
     },
   }
 }
+
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -17,7 +17,15 @@ return require('packer').startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'nvim-treesitter/playground'
+
+  -- use 'neovim/nvim-lspconfig'
+  -- use 'kabouzeid/nvim-lspinstall'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
 
 end)
