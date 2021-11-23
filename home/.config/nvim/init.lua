21c80606772b26ede46873a2c585f9447b6029cd
Chad Capra <chadcapra@gmail.com>
2021-11-23 10:52:57 -0600

    fix leader shortcuts by placing mapping first

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,11 +1,11 @@
 -- Sensible defaults
 require('settings')
 
+-- Load mappings
+require('mappings')
+
 -- Load plugins
 require('plugins')
 
 -- Load config
 require('config')
-
--- Load mappings
-require('mappings')
