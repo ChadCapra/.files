63fc988957f3f5c04c469344b8012fabd650332b
Chad Capra <chadcapra@gmail.com>
2022-01-14 11:40:11 -0600

    remove custom navigation mappings, not comp. w/ luasnip

--- home/.config/nvim/lua/ccaps/mappings.lua
+++ home/.config/nvim/lua/ccaps/mappings.lua
@@ -1,18 +1,6 @@
 local mappings = {
   {'n', '<Space>', ''},
 
-  -- remap navigation for better layouts
-  {'', 'n', 'j'},
-  {'', 'N', '}'},
-  {'', '<C-n>', '<C-d>'},
-  {'', 'l', 'k'},
-  {'', 'L', '{'},
-  {'', '<C-l>', '<C-u>'},
-
-  {'', 'j', 'l'},
-  {'', 'k', 'h'},
-
-
   -- File/Buffer/Window navigation
   {'n', '<leader>w', '<C-w>w'},
   {'n', '<leader>W', '<C-w>W'},
@@ -23,9 +11,6 @@ local mappings = {
 
 
   -- Search/Replace (from visual mode or last search},
-  {'n', 'h', 'n'},  -- use h (highlight) for repeated search
-  {'n', 'H', 'n'},  -- use h (highlight) for repeated search
-
   {'v', '<leader>r', '"ry:%s/<C-r>r//gc<left><left><left>'},
   {'v', '<leader>R', '"ry:%s/<C-r>R//g<left><left>'},
 
@@ -48,18 +33,17 @@ local mappings = {
 
 
   -- Move text up and down
-  {"n", "<A-n>", ":m .+1<cr>=="},
-  {"n", "<A-l>", ":m .-2<cr>=="},
-  {"i", "<A-n>", "<Esc>:m .+1<cr>==gi"},
-  {"i", "<A-l>", "<Esc>:m .-2<cr>==gi"},
-  {"v", "<A-n>", ":m '>+1<cr>gv=gv"},
-  {"v", "<A-l>", ":m '<-2<cr>gv=gv"},
+  {"n", "<A-j>", ":m .+1<cr>=="},
+  {"n", "<A-k>", ":m .-2<cr>=="},
+  {"i", "<A-j>", "<Esc>:m .+1<cr>==gi"},
+  {"i", "<A-k>", "<Esc>:m .-2<cr>==gi"},
+  {"v", "<A-j>", ":m '>+1<cr>gv=gv"},
+  {"v", "<A-k>", ":m '<-2<cr>gv=gv"},
 
 
   -- Visual mode helpers
   {"v", "<", "<gv"},
   {"v", ">", ">gv"},
-  {"v", "p", '"_dp'},
 
 
   -- Edit/Reload vimrc
