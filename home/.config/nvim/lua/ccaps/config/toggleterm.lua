fd725d448a5d1ab147514ebf3ea3b4a8258091d9
Chad Capra <chadcapra@gmail.com>
2022-03-29 15:55:15 -0500

    toggle lazygit

--- home/.config/nvim/lua/ccaps/config/toggleterm.lua
+++ home/.config/nvim/lua/ccaps/config/toggleterm.lua
@@ -1,12 +1,35 @@
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  }
+})
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+local maps = {
+
+  --File Explorer
+  {'n', '<leader>g', [[<cmd>lua _LAZYGIT_TOGGLE()<CR>]]},
+}
+
+require(NS .. '.utils').set_mappings(maps)
+
 local M = {}
 
 M.config  = function ()
   require("toggleterm").setup{
-     -- size can be a number or function which is passed the current terminal
-  size = 20,
-  open_mapping = [[<c-\>]],
-  shell = 'fish',
+    -- size can be a number or function which is passed the current terminal
+    size = 20,
+    open_mapping = [[<c-\>]],
+    shell = 'fish',
   }
+
 end
 
 return M
