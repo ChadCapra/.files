cf800528402eabee4e9b4159c55e80e9132c95b0
Chad Capra <chadcapra@gmail.com>
2022-04-17 15:34:55 -0500

    init packer on first load

--- home/.config/nvim/lua/ccaps/plugins.lua
+++ home/.config/nvim/lua/ccaps/plugins.lua
@@ -53,9 +53,11 @@ local plugins = {
 -- install packer if not exists
 local packer_bootstrap
 local fn = vim.fn
+local execute = vim.api.nvim_command
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
 end
 
 
