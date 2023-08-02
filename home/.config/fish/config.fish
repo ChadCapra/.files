494d9c57898a3b01f7cb7b04ba513412ca318079
Chad Capra <chadcapra@gmail.com>
2023-08-02 18:36:25 -0500

    asdf and flyctl

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,4 +1,7 @@
 fish_add_path /home/chadcapra/.mix/escripts
 fish_add_path /home/chadcapra/.local/bin
+fish_add_path /home/chadcapra/.fly/bin
+
+source /opt/asdf-vm/asdf.fish
 
 starship init fish | source
