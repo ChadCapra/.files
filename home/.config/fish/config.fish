05429d0a3243823cac2a2bb28e97b6cb769b773d
Chad Capra <chadcapra@gmail.com>
2023-10-04 10:40:17 -0500

    update elixir

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,4 @@
+fish_add_path /home/chadcapra/.asdf/installs/elixir/1.14.0/.mix/escripts
 fish_add_path /home/chadcapra/.mix/escripts
 fish_add_path /home/chadcapra/.local/bin
 fish_add_path /home/chadcapra/.fly/bin
